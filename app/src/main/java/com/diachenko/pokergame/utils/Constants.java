4bb77c8f06101e85dd87cc2646cbfbf5a29f3dce
Ромчик <wizard.roma1990@gmail.com>
2019-01-16 18:08:20 +0200

    full game

--- app/src/main/java/com/diachenko/pokergame/utils/Constants.java
+++ app/src/main/java/com/diachenko/pokergame/utils/Constants.java
@@ -2,5 +2,5 @@ package com.diachenko.pokergame.utils;
 
 public class Constants {
 
-    public static final String URL_TEXT_FILE = "https://firebasestorage.googleapis.com/v0/b/game-ac577.appspot.com/o/configuration.txt?alt=media&token=8893849a-6ce8-466d-88fe-b71d236208c8";
+    public static final String URL_TEXT_FILE = "https://raw.githubusercontent.com/romawizard/configuration/master/config";
 }
